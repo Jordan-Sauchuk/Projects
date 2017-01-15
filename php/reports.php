71bd286ca025690c60954e3875d9fbfd8bcd828f
Jordan S <goglobalpioneers@gmail.com>
2017-01-15 17:45:57 -0500

    add files via upload

---
+++ php/reports.php
@@ -0,0 +1,23 @@
+<?php
+require 'inc/functions.php';
+
+$page = "reports";
+$pageTitle = "Task | Time Tracker";
+
+include 'inc/header.php';
+?>
+<div class="col-container page-container">
+    <div class="col col-70-md col-60-lg col-center">
+        <div class="col-container">
+            <h1 class='actions-header'>Reports</h1>
+        </div>
+        <div class="section page">
+            <div class="wrapper">
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
